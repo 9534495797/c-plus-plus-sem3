a1216355a946e5605500f28dfb4b66bb33ccb692
9534495797 <chaubeyankit927@gmail.com>
2023-08-05 07:33:12 +0530

    ok

---
+++ sum.cpp
@@ -0,0 +1,42 @@
+//#include<iostream>
+//int main(){
+//	int a=4,b=6;
+//	std::cout<<a+b;
+//}
+
+
+
+//#include <iostream>
+//
+//int main() {
+//    int num1, num2, sum;
+//
+//    // Input the first number
+//    std::cout << "Enter the first number: ";
+//    std::cin >> num1;
+//
+//    // Input the second number
+//    std::cout << "Enter the second number: ";
+//    std::cin >> num2;
+//
+//    // Add the two numbers
+//    sum = num1 + num2;
+//
+//    // Display the result
+//    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+//
+//    return 0;
+//}
+
+
+#include<iostream>
+int main(){
+	int num1,num2,diff;
+	std::cout<<"enter the first num =";
+	std::cin>>num1;
+	std::cout<<"enter the second  num =";
+	std::cin>>num2;
+	diff=num1-num2;
+	std::cout<<"the diff of"<<num1 << "and" << num2 << "is = "<<diff<<std::endl;
+	
+}
