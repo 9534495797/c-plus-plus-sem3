55a95da86272ef89a33e3f90c9e295824e66892f
9534495797 <chaubeyankit927@gmail.com>
2023-08-07 19:58:32 +0530

    ok

---
+++ DSA.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a[5],i;
+	printf("enter arrays elements ");
+	for(i=0;i<5;i++){]
+	scanf("%d",&a[i]);
+	}
+	printf("entered array elements are: ");
+	for(i=0;i<5;i++){
+		printf("%d"a[i]);
+		
+	}
+	getch();
+}
