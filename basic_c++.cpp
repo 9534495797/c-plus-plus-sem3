ee5d98f0985dcd3c5f66320cd246b740efeb4bf3
9534495797 <chaubeyankit927@gmail.com>
2023-08-08 07:23:48 +0530

    ok

---
+++ basic_c++.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	cout<<"hello world";
+//	return 0;
+//}
+
+
+//int main(){
+//	int arr[]={1,2,7,12,9};
+//	int even=0;
+//	int odd=0;
+//	int length=sizeof(arr)/sizeof(arr[0]);
+//	for(int i=0;i<length;i++){
+//		if(arr[i]%2==0){
+//			even++;
+//		}
+//		else{
+//			odd++;
+//		}
+//		
+//	}
+//	cout<<"even numbers : "<<even<<endl;
+//	cout<<"odd n umbers : "<<odd<<endl;
+//}
+
+
+//void printfunction(){
+//	int i=1;
+//	while(i<=20){
+//		cout<<i<<" ";
+//		i++;
+//	}
+//}
+//int main(){
+//	printfunction();
+//	return 0;
+//}
+
+
+//int main(){
+//	string s;
+//	cout<<"enter a string = ";
+//	getline(cin,s);
+//	cout<<"you have entered ";
+//	cout<<s<<endl;
+//	s.push_back('y');//add at end of string
+//	cout<<"after using push back "<<s<<endl;
+//	s.pop_back();
+//	cout<<"after using pop back"<<s<<endl;
+//	return 0;
+//}
