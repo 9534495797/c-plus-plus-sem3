d953495dfeb5dfa5ec73d364ce8c22fba0142b30
9534495797 <chaubeyankit927@gmail.com>
2023-08-09 09:27:43 +0530

    ok

---
+++ c++day1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	
+//	int numl=70;
+//	double num2=256.783;
+//	char ch='a';
+//	cout<<numl<<endl;//insertion operator
+//	cout<<num2<<endl;
+//	cout<<"your  "<<"character "<<"is = "<<ch<<endl;
+//	cout<<"I AM THE KING "<<"name is ANKIT CHAUBEY"<<endl;
+//}
+
+
+int main(){
+	int num;
+	cout<<"input an integer: ";
+	cin>>num;//extraction operator
+	cout<<"the number is: "<<num;
+}
