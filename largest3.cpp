6fe73e6d140cde8e23442c06dc7a20d318304697
9534495797 <chaubeyankit927@gmail.com>
2023-08-10 20:21:43 +0530

    ok

---
+++ largest3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int num1,num2,num3;
+//	cout<<"first number = ";
+//	cin>>num1;
+//	cout<<"second number = ";
+//	cin>>num2;
+//	cout<<"third number = ";
+//	cin>>num3;
+//	if(num1>num2 && num1>num3){
+//		cout<<"num1 is largest";
+//	}
+//	else if(num2>num1 && num2>num3){
+//		cout<<"num2 is largest";
+//	}
+//	else if(num3>num1 && num3>num2){
+//		cout<<"num3 is largest";
+//	}
+//	else{
+//		cout<<"invalid condition";
+//	}
+//		
+//	
+//}  //largest among 3
