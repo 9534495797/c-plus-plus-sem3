fb25c017817d7bf2677a92bc8e59b69f4f076e55
9534495797 <chaubeyankit927@gmail.com>
2023-08-10 18:41:43 +0530

    ok

---
+++ top4question.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int num1,num2,num3;
+//	cout<<"first number = ";
+//	cin>>num1;
+//	cout<<"second number = ";
+//	cin>>num2;
+//	cout<<"third number = ";
+//	cin>>num3;
+//	if(num1>num2 && num1>num3){
+//		cout<<"num1 is largest";
+//	}
+//	else if(num2>num1 && num2>num3){
+//		cout<<"num2 is largest";
+//	}
+//	else if(num3>num1 && num3>num2){
+//		cout<<"num3 is largest";
+//	}
+//	else{
+//		cout<<"invalid condition";
+//	}
+//		
+//	
+//}  //largest among 3
+
+//int main(){
+//	int num,sum=0,i;
+//	cout<<"enter number for sum\n= ";
+//	cin>>num;
+//	for(i=0;i<=num;i++){
+//		sum+=i;
+//	}
+//	cout<<sum;
+//} //sum of natural number
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    cout << "Enter the number of terms: ";
+//    cin >> n;
+//
+//    int fib[n];
+//    fib[0] = 0;
+//    fib[1] = 1;
+//
+//    for (int i = 2; i < n; i++) {
+//        fib[i] = fib[i - 1] + fib[i - 2];
+//    }
+//
+//    cout << "Fibonacci Series up to " << n << " terms:" << endl;
+//    for (int i = 0; i < n; i++) {
+//        cout << fib[i] << " ";
+//    }
+//    
+//}
+
+
+
+//int main(){
+//	int num,i;
+//	cout<<"enter your num= ";
+//	cin>>num;
+//	
+//	int fibo[num];
+//	fibo[0]=0;
+//	fibo[1]=1;
+//	for(i=2;i<num;i++){
+//		fibo[i]=fibo[i-1]+fibo[i-2];
+//	}
+//	
+//	for(i=0;i<num;i++){
+//		cout<<fibo[i]<<" ";
+//	}
+//	
+//}
