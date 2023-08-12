961b01003570375d9de5ea3661cdb9905b72d230
9534495797 <chaubeyankit927@gmail.com>
2023-08-12 08:01:20 +0530

    ok

---
+++ AB.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a ;
+	double b;
+	 char c;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cout<<"employee id"<<a<<endl;
+	cout<<"salary"<<b<<endl;
+	cout<<"department id"<<c<<endl;
+}
