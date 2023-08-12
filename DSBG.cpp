97de91a076d04efc93ca1988ef89a059f2a30b77
9534495797 <chaubeyankit927@gmail.com>
2023-08-12 08:02:00 +0530

    ok

---
+++ DSBG.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a , double b, char c;
+	cin>>a>>endl;
+	cin>>b>>endl;
+	cin>>c>>endl;
+	cout<<"employee id"<<a<<endl;
+	cout<<"salary"<<b<<endl;
+	cout<<"department id"<<c<<endl;
+}
