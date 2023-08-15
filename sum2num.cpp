91f89471fa11a655d2a26470e7916f354b0da219
9534495797 <chaubeyankit927@gmail.com>
2023-08-15 06:28:26 +0530

    ok

---
+++ sum2num.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	float num1,num2,add,diff,multi,division,modulo;
+//	cout<<"enter  first num: ";
+//	cin>>num1;
+//	cout<<"enter  second num: ";
+//	cin>>num2;
+//	add=num1+num2;
+//	diff=num1-num2;
+//	multi=num1*num2;
+//	division=num1/num2;
+//	modulo=num1%num2;
+//	cout<<"addition of your two number is = "<<add<<endl;
+//	cout<<"difference of your two number is = "<<diff<<endl;
+//	cout<<"multiplication of your two number is = "<<multi<<endl;
+//	cout<<"division of your two number is = "<<division<<endl;
+//	cout<<"modulo of your two number is = "<<modulo<<endl;
+	
+//}
+
+
