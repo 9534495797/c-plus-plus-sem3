9a53423dcce8b37eb91dc331fffda328470eac17
9534495797 <chaubeyankit927@gmail.com>
2023-08-24 16:02:52 +0530

    ok

---
+++ union.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//union data{
+//int intvalue;
+//float floatvalue;
+//char charvalue;
+//
+//
+//}mydata;
+//int main(){
+//	mydata.intvalue=45;
+//	cout<<"int value is:"<<mydata.intvalue<<endl;
+//	mydata.floatvalue=45.6;
+//	cout<<"float value is:"<<mydata.floatvalue<<endl;
+//	mydata.charvalue='A';
+//	cout<<"char value is:"<<mydata.charvalue<<endl;
+//	cout<<sizeof(data);
+//	return 0;
+	
+	
+//}
