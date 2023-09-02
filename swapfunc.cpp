01849a5aae36f2095e39dea092c770148064d251
9534495797 <chaubeyankit927@gmail.com>
2023-09-02 05:59:21 +0530

    ok

---
+++ swapfunc.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+//void swap(int *x,int *y){
+//	int temp;
+//	temp=*x;
+//	*x=*y;
+//	*y=temp;
+//}
+//int main(){
+//	int a=10;
+//	int b=20;
+//	swap(&a,&b);
+//	cout<<"a="<<a<<endl;
+//	cout<<"b="<<b<<endl;
+//	return 0;
+//}
+
+//explorre
+int main(){
+	int a=10;
+	//int b=20;
+	//swap(&a,&b);
+	cout<<"a="<<a<<endl;
+	cout<<"a="<<&a<<endl;
+	cout<<"a="<<a<<endl;
+	//cout<<"b="<<b<<endl;
+	return 0;
+}
