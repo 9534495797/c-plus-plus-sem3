bb18428f783f3b7d7872fa213aa3f879b8948146
9534495797 <chaubeyankit927@gmail.com>
2023-09-09 06:37:47 +0530

    ok

---
+++ string.cpp
@@ -0,0 +1,73 @@
+ #include<iostream>
+ using namespace std;
+//int main(){
+//	string s1("man");
+//	string s2="beast";
+//	string s3;
+//	s3=s1;
+//	cout<<"s3"<<s3<<endl;
+//	s3="neither"+s1+"nor";
+//	s3+=s2;
+//	cout<<"s3"<<s3<<endl;
+//	s1.swap(s2);
+//	cout<<s1<<"nor"<<s2<<endl;
+//	return 0;
+//}
+
+//int main(){
+//	string str="welcomee";
+//	cout<<"size"<<str.size()<<endl;
+//	cout<<"length"<<str.length()<<endl;
+//	cout<<"max size"<<str.max_size()<<endl;
+//	cout<<"empty"<<(str.empty())<<endl;
+//	}
+
+
+//compare
+//int main(){
+//	string s1="bcme";
+//	string s2="abcrome";
+//	cout<<s1.compare(s2)<<endl;
+//	cout<<s1.compare(2,2,s2,5,2);
+//	return 0;
+//}
+
+//find()
+//int main(){
+//	string s1="arlcome";
+//	string s2="wearomea";
+//	cout<<s1.find("lc")<<endl;
+//	cout<<s2.rfind("ea");//rfind means find last
+//	cout<<s2.find_last_of('e');
+//}
+
+//substr()
+//int main(){
+//	string s1="welcome";
+//	string s2;
+//	s2=s1.substr(0,2);
+//	cout<<s2;
+//	return 0;
+//}
+
+//int main(){
+//	string s1="Ankit";
+//	cout<<s1<<endl;
+//	//cout<<typeof(s1)<<endl;
+//	cout<<s1[0]<<endl;
+//	//cout<<s1[1:4]<<endl;
+//	cout<<s1.length()<<endl;
+//	cout<<s1.size()<<endl;
+//	
+//}
+
+
+int main(){
+	string s1="Ankit";
+	string s2="Chaubey";
+	cout<<s1<<endl;
+	cout<<s2<<endl;
+	cout<<s1+s2<<endl;
+	cout<<s1+" "+s2<<endl;
+
+}
