51adcd01bd5cf47ebea75e5bfc24b73819a626a4
9534495797 <chaubeyankit927@gmail.com>
2023-09-09 06:38:47 +0530

    ok

---
+++ swapall.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int x=4;
+//	int y=5;
+//	int temp=x;
+//	x=y;
+//	y=temp;
+//	cout<<"x= "<<x<<endl;;
+//	cout<<"y= "<<y;
+//}
+
+//2nd method
+//int main(){
+//	int x=4;
+//	int y=5;
+//	x=x+y;
+//	y=x-y;
+//	x=x-y;
+//	cout<<x<<endl;
+//	cout<<y;
+//	
+//}
+
+
+// 3 variable swaping
+//int main(){
+//	int a=1;
+//	int b=2;
+//	int c=3;
+//	a=b;
+//	b=c;
+//	c=c-a;
+//	cout<<a<<endl;
+//	cout<<b<<endl;
+//	cout<<c;
+//}
