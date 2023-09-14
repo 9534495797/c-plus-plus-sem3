8f0171031168e272f8f286a317cf260dc8849cb9
9534495797 <chaubeyankit927@gmail.com>
2023-09-14 20:32:30 +0530

    ok

---
+++ poijnter.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int num=10;
+//	int *ptr;
+//	ptr=&num;
+//	cout<<"address= "<<ptr<<endl;
+//	cout<<"value= "<<*ptr<<endl;
+//	return 0;
+//}
+int main(){
+	int x=10;
+	int *p;
+	p=&x;
+	cout<<p<<endl;
+	cout<<*p<<endl;
+}
