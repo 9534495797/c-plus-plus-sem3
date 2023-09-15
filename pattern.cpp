14c2fe1d27ef2ab43c0961341daa499b11310d24
9534495797 <chaubeyankit927@gmail.com>
2023-09-15 18:55:59 +0530

    ok

---
+++ pattern.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+//rectangle
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=n;j++){
+//			cout<<"*"<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+//right angle triangle
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=i;j++){
+//			cout<<"*"<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+//1 1 1 1 1 
+//2 2 2 2 2 
+//3 3 3 3 3 
+//4 4 4 4 4 
+//5 5 5 5 5   pattern
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=n;j++){
+//			cout<<i<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+
+
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5  pattern
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=n;j++){
+//			cout<<j<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+//1
+//2 2
+//3 3 3
+//4 4 4 4
+//5 5 5 5 5  pattern
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=i;j++){
+//			cout<<i<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+
+
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5 pattern
+//int main(){
+//	int n,i,j;
+//	cin>>n;
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=i;j++){
+//			cout<<j<<" ";
+//		}
+//		cout<<endl;
+//	}
+//}
+
+
