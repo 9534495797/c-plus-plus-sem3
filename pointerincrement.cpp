887968fd3dcb93ecb351be1bd163384296f657b1
9534495797 <chaubeyankit927@gmail.com>
2023-09-16 06:29:22 +0530

    ok

---
+++ pointerincrement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int x=10;
+//	int *ptr;
+//	ptr=&x;
+//	cout<<x<<endl;
+//	cout<<ptr<<endl;
+//	*ptr+=1;
+//	cout<<x<<endl;
+//	cout<<ptr;
+//	return 0;
+//}
+
+int main(){
+	int x=10;
+	int *ptr;
+	ptr=&x;
+	cout<<x<<endl;
+	*ptr+=1;
+	cout<<x<<endl;
+	
+	
+	
+}
