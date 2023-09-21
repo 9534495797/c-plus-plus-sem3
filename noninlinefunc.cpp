dd446757526a357c44d3bd2a70ab4bee9455f302
9534495797 <chaubeyankit927@gmail.com>
2023-09-21 05:59:41 +0530

    ok

---
+++ noninlinefunc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int square(int s){
+	return s*s;
+}
+int main(){
+	cout<<"enter num for square is 5: "<<endl;
+	cout<<"square is: "<<square(5)<<endl;
+	return 0;
+}
