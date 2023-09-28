cd33d304fc308bf3e8828907e639247e7bcfca85
9534495797 <chaubeyankit927@gmail.com>
2023-09-28 05:38:12 +0530

    ok

---
+++ operator_overloading.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+//class abc{
+//	int x,y,z;
+//	public:
+//		abc(int a,int b,int c){
+//			x=a;
+//			y=b;
+//			z=c;
+//		}
+//		void display(){
+//			cout<<x;
+//			cout<<y;
+//			cout<<z<<"\n";
+//		}
+//		void operator -(){
+//			x=-x;
+//			y=-y;
+//			z=-z;
+//		}
+//};
+//int main(){
+//	abc s(10,-20,30);
+//	cout<<"s";
+//	s.display();
+//	-s;
+//	cout<<"s:";
+//	s.display();
+//	return (0);
+//}
+
+
+
+//class point{
+//	int x,y;
+//	public:
+//		point(){
+//			x=0;y=0;
+//			
+//		}
+//		point(int i,int j){
+//			x=i;y=j;
+//		}
+//		point operator -();
+//		void display(){
+//			cout<<x<<"\t"<<y;
+//		}
+//};
+//point point::operator -(){
+//	x=-x;
+//	y=-y;
+//}
+//int main(){
+//	point o1(10,10);
+//	-o1;//to negate the object
+//	o1.display();
+//}
+
+
+
+
+//overloading unary operator
+//class score{
+//	int val;
+//	public:
+//		score(){
+//			val=0;
+//		}
+//		void operator ++(){
+//			val=val+1;
+//		}
+//		int show(){
+//			return (val);
+//		}
+//};
+//int main(){
+//	score 
+//}
+
