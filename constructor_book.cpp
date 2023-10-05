ae7aa079df29756eb4e211bcb9acee3e7c72b23f
9534495797 <chaubeyankit927@gmail.com>
2023-10-05 17:47:51 +0530

    ok

---
+++ constructor_book.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+//class distance{
+//	int feet;
+//	float inches;
+//	public:
+//		distance():feet(0),inches(0.0){//constructor no arg
+//		}
+//		distance(int ft,float in):feet(ft),inches(in){//constructor two arg
+//		}
+//		void getdist(){
+//			cout<<"feet: ";
+//			cin>>feet;
+//			cout<<"inches: ";
+//			cin>>inches;
+//		}
+//		void showdist(){
+//			cout<<feet<<"\'-"<<inches<<'\"';
+//			
+//		}
+//		void add_dist(distance,distance);//declaration
+//};
+//void distance::add_dist(distance d2,distance d3){//add length d2 qnd d3// used:: because memebr functio define outside the class
+//
+//inches=d2.inches+d3.inches;
+//feet=0;
+//if(inches>=12.0){
+//	inches -=12.0;
+//	feet++;
+//}
+//feet +=d2.feet+d3.feet;
+//	
+//}
+//int main(){
+//	distance dist1,dist3;//definte two lengths
+//	distance dist2(11,6.25);//define and initilize dist2
+//	dist1.getdist();//get dist1 from user
+//	dist3.add_dist(dist1,dist2);//dist3=dist1+dist2
+//	//display all lengths
+//	cout<<"\ndist1: ";dist1.showdist();
+//	cout<<"\ndist2: ";dist2.showdist();
+//	cout<<"\ndist3: ";dist3.showdist();
+//	cout<<endl;
+//	return 0;
+//}
+
