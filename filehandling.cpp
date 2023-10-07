7d59fe16be3b27ffac7437eac59f57070b0b4d9b
9534495797 <chaubeyankit927@gmail.com>
2023-10-07 17:25:18 +0530

    ok

---
+++ filehandling.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<fstream>//file library to read and write data
+using namespace std;
+
+//int main(){
+//	fstream myfile;//myfile variable
+//	myfile.open("ankit.txt",ios::out);//two parameters file name and mode
+//	//this is write mode
+//	if(myfile.is_open()){
+//		cout<<"hello world"<<"\n";//write in file and 
+//cout<<"this is secomnd line";
+//myfile.close();//to close  the file
+//
+//
+//	}
+//	myfile.open("ankit.txt",ios::app);//two parameters file name and mode
+//	//this is append mode
+//	if(myfile.is_open()){
+//		cout<<"hello ankit";//write in file and 
+//
+//myfile.close();//to close  the file
+//}
+//	system("pause>0");
+//}
+//int main(){
+//	fstream myfile;
+//	myfile.open("ab.txt",ios::out);
+//	if(myfile.is_open()){
+//		cout<<"hello world";
+//		myfile.close();
+//	}
+//}
+
+//#include<string>//for getline unc
+////file read
+//int main(){
+//	fstream myfile;
+//	myfile.open("ankit.txt",ios::in);
+//	//read mode
+//	if(myfile.is_open()){
+//		string line;
+//		while(getline(myfile,line)){//it will read and store in line var
+//		
+//			cout<<line<<endl;
+//		}
+//		myfile.close();
+//	}
+//	
+//}
+
+
+
+//int main(){
+//	fstream myfile;
+//	myfile.open("a.txt",ios::out){
+//		if(myfile.is_open()){
+//			cout<<"hello world ";
+//		}
+//		myfile.close();
+//	}
+//}
+
+
+//class notes from ppt
+
