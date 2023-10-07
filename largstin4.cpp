bcf06f88f2ef408923f3d63ed79eeb0e4de4eb88
9534495797 <chaubeyankit927@gmail.com>
2023-10-07 17:28:30 +0530

    oik

---
+++ largstin4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int main() {
+    int a, b, c, d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    if(a>b&&a>c&&a>d){
+        cout<< a;
+    }else if(b>a&&b>c&&b>d){
+        cout<< b;
+    }else if(c>a&&c>b&&d){
+        cout<< c;
+    }else{
+        cout<< d;
+    }
+    
+    return 0;
+}
