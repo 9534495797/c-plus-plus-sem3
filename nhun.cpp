b539357f2896f3f3f6f1f93008153fafd65f4347
9534495797 <chaubeyankit927@gmail.com>
2023-10-23 06:51:29 +0530

    ok

---
+++ nhun.cpp
@@ -0,0 +1,28 @@
+
+//#include<iostream>
+//using namespace std;
+//class EVEN{
+//	public:
+//		int n,m;
+//		void inp(){
+//			int num,i;
+//			int x=n;
+//			int y=m;
+//			cout<<"Enter your lowest no:";
+//			cin>>n;
+//			cout<<"Enter your highest  no:";
+//			cin>>m;
+//			for(i=n;i<=m;i++){
+//				if(i%2==0){
+//					cout<<" "<<i<<endl;
+// 
+//					
+//				}
+//			}
+//		}
+//};
+//int main(){
+//	EVEN obj;
+//	obj.inp();
+//	return 0;
+//}
