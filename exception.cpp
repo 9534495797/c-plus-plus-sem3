1f9139f75d98524abbffebd40b256df97f1be5a2
9534495797 <chaubeyankit927@gmail.com>
2023-11-03 19:28:08 +0530

    ok

---
+++ exception.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int a,b;
+//	cout<<"enter values of a and b: ";
+//	cin>>a;
+//	cin>>b;
+//	int x=a-b;
+//	try{
+//		if(x!=0){
+//			cout<<"result(a/x)="<<a/x;
+//		}else{
+//			{
+//				throw (x);
+//			}
+//		}
+//		catch(int i){
+//			cout<<"exception caught:x="<<x;
+//		}
+//	}
+//	cout<<endl;
+//	return 0;-
+//}
+
+
+
+
+//q2
+//int main(){
+//	int a[5]={1,2,3,4,5};
+//	try{
+//		int i=0;
+//		while(1){
+//			cout<<a[i]<<endl;
+//			i++;
+//			if(i==5){
+//				throw i;
+//			}
+//		}
+//		catch(int j){
+//			cout<<"array out of bound"<<<j;
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//q3
+//void test(int x){
+//	try{
+//		if(x==0)throw x;
+//		if(x==-1)throw 'x';
+//		if(x==1)throw 1.0;
+//	}
+//	catch(....){
+//		cout<<"caught an exception";
+//	}
+//}
+//int main(){
+//	cout<<"testing";
+//	test(-1);
+//	test(0);
+//	test(1);
+//	return 0;
+//}
+
+
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+using namespace std;
+
