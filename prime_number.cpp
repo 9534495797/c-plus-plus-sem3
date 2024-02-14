d89361ad110636dbcd2ad8b392d92d7f8a0067a1
9534495797 <chaubeyankit927@gmail.com>
2024-02-14 11:15:57 +0530

    ok

---
+++ prime_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	bool isprime=1;
+//	int num;
+//	cin>>num;
+//	for(int i=2;i<=num;i++){
+//		if(num%i==0){
+//			isprime=0;
+//		}
+//		break;
+//	}
+//	if(isprime==1){
+//		cout<<num<<" "<<"is a prime number";
+//	}else{
+//		cout<<num<<" "<<"is not a prime number";
+//	}
+//}
+
+
+
